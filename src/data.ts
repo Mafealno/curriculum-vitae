dbea1b84a9a576533c4b2661f153df25fb1dc576
mafealno <mafealno@gmail.com>
2023-01-21 00:20:36 -0300

    feat: criado componente de informações do site

---
+++ src/data.ts
@@ -0,0 +1,13 @@
+
+
+import Section from "./interfaces/section";
+
+export const sections = [
+    { id: 1, name: "Inicio", description: "" },
+    { id: 2, name: "Sobre Mim", description: "" },
+    { id: 3, name: "Habilidades", description: "" },
+    { id: 4, name: "Experiência", description: "" },
+    { id: 5, name: "Acadêmico", description: "" },
+    { id: 6, name: "Portifólio", description: "" },
+    { id: 7, name: "Me Contate", description: "" }
+] as Section[];
