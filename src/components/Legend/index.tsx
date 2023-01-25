b925a095a5dd0027997385663b4cb0d0dc35e3f7
mafealno <mafealno@gmail.com>
2023-01-25 11:09:58 -0300

    fix: ajuste estilo do componte

--- src/components/Legend/index.tsx
+++ src/components/Legend/index.tsx
@@ -3,17 +3,27 @@ import React, { useState } from 'react'
 type Props = {
     children: JSX.Element,
     label: string,
+    className?: string,
+    position?: number,
 }
 
 function Legend(props: Props) {
 
     const [showLegend, setShowLegend] = useState(false);
 
-    const { children, label } = props
+    const { children, label, className, position } = props
 
   return (
-    <span className="relative flex justify-center items-center" onMouseEnter={() => setShowLegend(true)} onMouseLeave={() => setShowLegend(false)}>
-        {showLegend && <div className="absolute bg-black rounded py-2 px-4 text-white">{label}</div>}
+    <span
+    className={`relative flex h-full justify-center items-center ${className}`}
+    onMouseEnter={() => setShowLegend(true)}
+    onMouseLeave={() => setShowLegend(false)}
+    >
+        {showLegend &&
+          <div className="cursor-default z-10 max-w-[150px] text-center absolute bg-black rounded py-2 px-4 text-white"
+          style={{
+            transform: position ? `translateX(${position}%)`: ""
+          } as React.CSSProperties}>{label}</div>}
         {children}
     </span>
   )
