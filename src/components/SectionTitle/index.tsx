11281308e4b5de20470e35e73076adaec463924f
mafealno <mafealno@gmail.com>
2023-01-25 11:24:01 -0300

    fix: ajuste para adicionar item tem duvida ao lado do titulo

--- src/components/SectionTitle/index.tsx
+++ src/components/SectionTitle/index.tsx
@@ -1,19 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+import { FaQuestionCircle } from "react-icons/fa";
 
 import "./styles.css";
 
 type Props = {
-    title: string
+    title: string,
+    legend?: string
 }
 
 function SectionTitle(props: Props) {
 
-    const { title } = props;
+    const [showLegend, setShowLegend] = useState(false);
+
+    const { title, legend } = props;
 
   return (
     <>
+      <div className="flex flex items-center gap-3 relative">
         <p className="text-2xl font-bold">{title}</p>
-        <div className="line-title w-12 h-1 rounded mb-5"></div>
+        {legend && (
+          <>
+            <FaQuestionCircle className='h-6 w-6 text-[var(--secondary-color)]'
+            onMouseEnter={() => setShowLegend(true)}
+            onMouseLeave={() => setShowLegend(false)}
+            />
+            {showLegend && <div className="cursor-default text-center flex justify-center items-center z-30 bg-black rounded py-2 absolute left-48 px-4 text-white">{legend}</div>}
+          </>
+        )}
+      </div>
+      <div className="line-title w-12 h-1 rounded mb-5"></div>
     </>
   )
 }
