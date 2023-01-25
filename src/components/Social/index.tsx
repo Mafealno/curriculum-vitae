e48c7979447555dcbe3d97cf6f1f2d6afe80d1cf
mafealno <mafealno@gmail.com>
2023-01-25 11:24:32 -0300

    fix: ajuste estilo e icone

--- src/components/Social/index.tsx
+++ src/components/Social/index.tsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react'
 
-import { TiSocialGithub, TiSocialInstagram, TiSocialLinkedin } from "react-icons/ti"
+import { TiSocialInstagram, TiSocialLinkedin } from "react-icons/ti"
 import { HiOutlineMail } from "react-icons/hi";
+import { BsGithub } from "react-icons/bs";
 import { BsWhatsapp } from "react-icons/bs";
 
 function Social() {
@@ -14,8 +15,8 @@ function Social() {
 
   return (
     <>
-        <a href="https://github.com/Mafealno"><TiSocialGithub className="h-11 w-11"/></a>
-        <a href="https://www.linkedin.com/in/marcelo-fernando-alves-nogueira-3b1562116/"><TiSocialLinkedin className="h-10 w-10"/></a>
+        <a href="https://github.com/Mafealno"><BsGithub className="h-8 w-7"/></a>
+        <a href="https://www.linkedin.com/in/marcelo-fernando-alves-nogueira-3b1562116/"><TiSocialLinkedin className="h-11 w-11"/></a>
         <a href="http://api.whatsapp.com/send?1=pt_BR&phone=5511949679592"><BsWhatsapp className="h-7 w-7"/></a>
         <a href="https://www.instagram.com/mafealno/"><TiSocialInstagram className="h-8 w-8"/></a>
         {showCopy && <span className="absolute bg-black rounded py-2 px-4 text-white">Copiado</span>}
